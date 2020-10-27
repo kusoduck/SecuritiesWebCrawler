43043e530620c7e97e4a85ac1895e7235d74f55d
sam <sam.chen@camline.com>
2020-10-27 18:04:04 +0800

    temp assembly code

--- src/SecuritiesWebCrawler.bat
+++ src/SecuritiesWebCrawler.bat
@@ -1,13 +1,34 @@
 @echo off
-echo path:%~dp0
+rem Batch file(based on SM-14) to install Windows service with LineWorks Service Manager
+rem =====================================================================================
+echo Start Report Template Migration Processor
 
-::取得bat檔案所在的當前目錄
-set base=%~dp0
+if "%JAVA_HOME%" == "" goto nojavahome
 
-set class=%base%\classes
-set libs=%base%\libs
+rem **********************************
+rem include project specific jar files
+rem **********************************
+set APP_HOME=%~dp0
+rem remove trailing slash
+IF %APP_HOME:~-1%==\ set APP_HOME=%APP_HOME:~0,-1%
 
-set class_path=%class%;%libs%;
+set CP=
+set CP=%CP%;${dependency.cp.windows}
+set CP=%CP%;lib\${project.build.finalName}.${project.packaging}
 
-java -cp %class_path% com.kusoduck.securities.SecuritiesWebCrawler
-@pause
+rem *********************************
+rem run class, command line options
+rem *********************************
+
+
+"%JAVA_HOME%"\bin\java.exe -cp %CP% com.kusoduck.securities.SecuritiesWebCrawler
+
+goto done
+
+:nojavahome
+echo.
+echo ERROR: No JAVA_HOME environment variable set.
+goto done
+
+:done
+echo bye...
